15e0182d5ae3195ed23cd72f364cea398a5e6998
Ayan Khandelwal <khandelwalayan11@gmail.com>
2025-06-16 13:20:33 +0530

    Basic frontend changes

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -6,13 +6,15 @@ import './index.css';
 import App from './App.jsx';
 import Home from './components/Home.jsx';
 import Room from './components/Room.jsx';
+import CodeEditor from './components/CodeEditor.jsx';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Home />} />
-        <Route path="/room/:roomId" element={<Room />} />
+        <Route path="/start" element={<Room />} />
+        <Route path="/room/:roomId" element={<CodeEditor />} />
       </Routes>
     </BrowserRouter>
   </StrictMode>
